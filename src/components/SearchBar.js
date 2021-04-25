66e5d316e0920e9871cb0f789c4fca76001b35a7
Daniel Son <dson.email@gmail.com>
2021-04-25 12:50:46 +1000

    End of 89. Form submit handling.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,13 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
+    onFormSubmit(event) {
+        event.preventDefault(); // or else it will refresh the page.
+    }
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <input 
