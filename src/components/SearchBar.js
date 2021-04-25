e46a4d499a47a732e4f7eb8cb705aeda26d07f14
Daniel Son <dson.email@gmail.com>
2021-04-25 13:08:20 +1000

    End of 91. "this" issues.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,15 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault(); // or else it will refresh the page.
+
+        console.log(this.state.term);
     }
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form" onSubmit={onFormSubmit}>
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
                         <input 
