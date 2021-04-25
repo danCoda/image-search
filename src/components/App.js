7b31c4e6294f9968f847a4acc70098616fee39e9
Daniel Son <dson.email@gmail.com>
2021-04-25 14:13:23 +1000

    End of 97. Async / await... Asynchronous calls, made via axios.

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
-    onSearchSubmit(term) {
+
+    async onSearchSubmit(term) {
         console.log(term);
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+                headers: {
+                    Authorization: 'Client-ID AGN8OW5A2DnoZBtOYUWPKeYTG7KmCw1dIr-b4f4iv14'
+                },
+                params: { query: term }
+            });
+        console.log(response.data.results);
     }
 
     render() {
