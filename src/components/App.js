5c7243f29ff22bc4e4152d9cbda0c5e475674f9d
Daniel Son <dson.email@gmail.com>
2021-04-25 14:20:57 +1000

    End of 99. 'this' issue resolved again.

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
+    state = { images: [] };
 
-    async onSearchSubmit(term) {
+    onSearchSubmit = async (term) => {
         console.log(term);
         const response = await axios.get('https://api.unsplash.com/search/photos', {
                 headers: {
@@ -12,13 +13,15 @@ class App extends React.Component{
                 },
                 params: { query: term }
             });
-        console.log(response.data.results);
+
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images.
             </div>
         );
     }
