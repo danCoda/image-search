3df703f53a73a0553293b65e03eda5dea60e51a0
Daniel Son <dson.email@gmail.com>
2021-04-25 14:40:48 +1000

    End of 105. Added images with IDs.

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,12 @@
 import react from "react";
 
 const ImageList = (props) => {
+    const images = props.images.map(({urls, description, id}) => {
+        return <img src={urls.regular} key={id} alt={description}/>
+    });
     return (
         <div>
-        
+            {images}
         </div>
     )
 };
