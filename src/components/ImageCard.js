d7e251c35ca343ca1b0918ae73e865a0cb0b7264
Daniel Son <dson.email@gmail.com>
2021-04-25 15:13:51 +1000

    End of 114. Grid working nicely.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    // We want to use the React ref system, to refer to images.
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+        this.state = { spans: 0 };
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans });
+    }
+
     render() {
         const { description, urls } = this.props.img;
 
+        // 
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img 
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
